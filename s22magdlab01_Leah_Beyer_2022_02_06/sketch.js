b1d33ff157938b059bb2e19c3d7d7539062a3926
BeyerLR30 <103956560+BeyerLR30@users.noreply.github.com>
2022-04-18 12:45:37 -0500

    Add files via upload

---
+++ s22magdlab01_Leah_Beyer_2022_02_06/sketch.js
@@ -0,0 +1,89 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  strokeWeight(6);
+  stroke(0);
+  fill(210);
+  rect(10,95, 70,310);
+  line(45,90, 45,70);
+  fill(255);
+  strokeWeight(9);
+  ellipse(45,60, 15,15);
+  
+  fill(100);
+  noStroke();
+  rect(25,130, 10,10);
+  rect(55,130, 10,10);
+  rect(25,170, 10,10);
+  rect(55,170, 10,10);
+  rect(25,210, 10,10);
+  rect(55,210, 10,10);
+  rect(25,250, 10,10);
+  rect(55,250, 10,10);
+  rect(25,290, 10,10);
+  rect(55,290, 10,10);
+  rect(25,330, 10,10);
+  rect(55,330, 10,10);
+  
+  stroke(230);
+  strokeWeight(40);
+  point(340,130);
+  
+  noStroke();
+  fill(190);
+  rect(290,140, 100,280);
+  
+  stroke(0);
+  strokeWeight(10);
+  strokeCap(ROUND);
+  line(320,170, 360,170);
+  stroke(0);
+  strokeCap(ROUND);
+  line(320,220, 360,220);
+  stroke(0);
+  strokeCap(ROUND);
+  line(320,270, 360,270);
+  
+  strokeWeight(3);
+  stroke(250);
+  fill(0);
+  ellipse(340, 380, 60,80);
+  
+  fill(0);
+  rect(110,70, 150, 450, 10,10,0,0);
+  
+  noFill();
+  stroke(255);
+  strokeWeight(2);
+  rect(130,100, 40,20);
+  rect(200,130, 40,20);
+  rect(130,160, 40,20);
+  rect(200,190, 40,20);
+  rect(130,220, 40,20);
+  rect(200,250, 40,20);
+  rect(130,280, 40,20);
+  rect(200,310, 40,20);
+  rect(130,340, 40,20);
+  
+  strokeWeight(2);
+  stroke(255);
+  point(10,40);
+  point(40,10);
+  point(70,30);
+  point(80,70);
+  point(120,50);
+  point(160,20);
+  point(200,45);
+  point(240,20);
+  point(280,50);
+  point(310,70);
+  point(320,30);
+  point(355,10);
+  point(360,55);
+  point(390,20);
+  
+ 
+}
